d23b77678c7add8cc92bcb9f25a3ef0a0ed110a8
ZeynepDuvarci <duvarcizey@gmail.com>
2023-03-07 13:37:09 +0300

    day11 update

--- day11/FaceDetection.py
+++ day11/FaceDetection.py
@@ -3,3 +3,25 @@ import mediapipe as mp
 import time
 
 cap =cv2.VideoCapture('day11/video.mp4')
+
+mpFaceDetection=mp.solutions.face_detection
+mpDraw=mp.solutions.drawing_utils
+faceDetection=mpFaceDetection.FaceDetection()
+
+while True:
+    success, img=cap.read()
+    imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results= faceDetection.process(imgRGB)
+    
+    if results.detections:
+        for id,detection in enumerate(results.detections):
+            bboxC=detection.location_data.relative_bounding_box
+            ih, iw, ic=img.shape
+            
+            bbox=int(bboxC.xmin*iw), int(bboxC.ymin*ih), \
+                int(bboxC.width*iw),int(bboxC.height*ih)
+            
+            cv2.rectangle(img,bbox,(255,0,255),2)    
+            
+    cv2.imshow('Image',img)
+    cv2.waitKey(1)
