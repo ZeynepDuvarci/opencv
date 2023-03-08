a8aa7ad92a1e3c46fdd8d4cdc81e5bfdb4dafba1
ZeynepDuvarci <duvarcizey@gmail.com>
2023-03-09 01:22:17 +0300

    day12 done

--- day12/HandTrackingModule.py
+++ day12/HandTrackingModule.py
@@ -4,7 +4,8 @@ import time
 
 
 class handDetector():
-    def __init__(self,mode=False,maxHands=2,complexity=1, detectionCon=0.5, trackCon=0.5):
+    
+    def __init__(self,mode=False, maxHands=2, complexity=1, detectionCon=0.5, trackCon=0.5):
         self.mode=mode
         self.maxHands=maxHands
         self.complexity=complexity
@@ -17,14 +18,27 @@ class handDetector():
 
     def findHands(self, img, draw=True):
         imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        results=self.hands.process(imgRGB)
+        self.results=self.hands.process(imgRGB)
         
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
-
+    
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList=[]
+        
+        if self.results.multi_hand_landmarks:
+            myHand=self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h,w,c=img.shape
+                cx, cy=int(lm.x*w),int(lm.y*h)
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img,(cx,cy),7,(0,255,0),cv2.FILLED)
+        
+        return lmList    
 
 def main():
     pTime=0
@@ -36,7 +50,11 @@ def main():
     while True:
         success, img= cap.read()
         img=detector.findHands(img)
-    
+        
+        lmList=detector.findPosition(img)
+        if len(lmList)!=0:
+            print(lmList[4])
+        
         cTime=time.time()
         fps=1/(cTime-pTime)
         pTime=cTime
@@ -47,5 +65,5 @@ def main():
         cv2.waitKey(1)
     
     
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
